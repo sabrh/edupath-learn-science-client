28c8e6cbeed9aae943b0242b0674559b13d36277
Sabrina Haque <sabrina.haque442@gmail.com>
2025-08-20 14:29:41 +0600

    project setup and add navbar

---
+++ src/app/tutorials/page.js
@@ -0,0 +1,8 @@
+export default function TutorialsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Tutorials</h1>
+      <p>Here you can find all tutorials.</p>
+    </div>
+  );
+}
